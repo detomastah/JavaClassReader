bc05d1311aed783a190fb888bdccb28b5955889d
Lukasz Pelszynski <lukaszp@lavabit.com>
2012-08-20 22:10:54 +0200

    First rewrite

--- class_file_items.rb
+++ class_file_items.rb
@@ -1,174 +1,261 @@
 require 'stringio'
 
-module JavaClassFile
-	class BaseInfo
-		def initialize(class_data, f)
-			@class_data = class_data
-		end
-	end
+module Java
+	module ClassFile
+		class BigEndianFile < File
+			def read_u4
+				self.read(4).unpack("N").first
+			end
 
-	class FieldRefInfo < BaseInfo
-		attr_accessor :class_index, :name_and_type_index
+			def read_u2
+				self.read(2).unpack("n").first
+			end
 
-		def initialize(class_data, f)
-			super
-			@class_index = f.read_u2
-			@name_and_type_index = f.read_u2
-		end
-	end
-
-	class MethodRefInfo < FieldRefInfo
-	end
-
-	class InterfaceMethodRefInfo < FieldRefInfo
-	end
-
-	class ClassInfo < BaseInfo
-		attr_accessor :name_index
-
-		def initialize(class_data, f)
-			super
-			@name_index = f.read_u2
-		end
-	end
-
-	class StringInfo < BaseInfo
-		attr_accessor :string_index
-
-		def initialize(class_data, f)
-			super
-			@string_index = f.read_u2
-		end
-	end
-
-	class Utf8Info < BaseInfo
-		attr_accessor :length, :bytes
-
-		def initialize(class_data, f)
-			super
-			@length = f.read_u2;
-			@bytes = f.read(@length)
-			#puts @bytes
-		end
-	end
-
-	class NameAndTypeInfo < BaseInfo
-		attr_accessor :name_index, :descriptor_index
-
-		def initialize(class_data, f)
-			super
-			@name_index = f.read_u2;
-			@descriptor_index = f.read_u2
-		end
-	end
-
-	class AttributeInfo < BaseInfo
-		def initialize(class_data, f)
-			super
-			@attribute_length = f.read_u4
-			read_info(f)
-		end
-
-		private
-		def read_info(f)
-			f.read(@attribute_length)
-		end
-	end
-
-	class CodeAttributeInfo < AttributeInfo
-		attr_accessor :max_stack, :max_locals, :code, :exception_table, :attributes
-
-		def read_info(f)
-			@max_stack = f.read_u2
-			@max_locals = f.read_u2
-			code_length = f.read_u4
-			@code = f.read(code_length)
-			exception_table_length = f.read_u2
-			f.read(exception_table_length)
-			attributes_count = f.read_u2
-			@attributes = []
-			attributes_count.times do
-				@attributes << AttributeInfoFactory.produce(self, @class_data, f)
+			def read_u1
+				self.read(1).unpack("C").first
 			end
 		end
-	end
+		
+		class ClassDefinition
+			attr_reader :constant_pool, :fields, :methods
 
-	class AttributeInfoFactory
-		def self.produce(caller_obj, class_data, f)
-			attribute_name_index = f.read_u2
-			attribute_name = class_data.constant_pool.fetch(attribute_name_index).bytes
-			begin
-				attribute_class = JavaClassFile.const_get(attribute_name + "AttributeInfo")
-			rescue
-				puts "\033[1;36m" + "No class #{attribute_name} for #{caller_obj.class}" + "\033[0m"
-				attribute_class = AttributeInfo
+			def read(file_path)
+				@file = BigEndianFile.open(file_path, "rb")
+
+				read_magic
+				@minor = @file.read_u2
+				read_major
+				@constant_pool_count = @file.read_u2
+				read_constant_pool
+				@access_flags = @file.read_u2
+				@this_class = @file.read_u2
+				@super_class = @file.read_u2
+				@interfaces_count = @file.read_u2
+				read_interfaces
+				@fields_count = @file.read_u2
+				read_fields
+				@methods_count = @file.read_u2
+				read_methods
+				@attributes_count = @file.read_u2
+				read_attributes
+
+				@file.close
 			end
-			attribute_class.new(class_data, f)
-		end
-	end
 
-	class FieldInfo < BaseInfo
-		attr_accessor :access_flags, :name_index, :descriptor_index, :attributes_count, :attributes
-		def initialize(class_data, f)
-			super
-			@access_flags = f.read_u2
-			@name_index = f.read_u2
-			@descriptor_index = f.read_u2
-			@attributes_count = f.read_u2
-			@attributes = []
-			@attributes_count.times do
-				@attributes << AttributeInfoFactory.produce(self, class_data, f)
+			private
+			def read_magic
+				@magic = @file.read_u4
+				raise "Not a class file" if @magic != 0xCAFEBABE
+			end
+
+			def read_major
+				@major = @file.read_u2
+				raise "Not Java SE 6 class" if @major != 0x32
+			end
+
+			def read_constant_pool
+				@constant_pool = [nil] #zero index is not used, since indexing starts from 1 to pool_count - 1
+				(@constant_pool_count - 1).times do
+					constant_type_id = @file.read_u1
+					constant = ItemInfo::ConstantInfoFactory.produce(constant_type_id, self, @file)
+					@constant_pool << constant
+				end
+			end
+
+			def read_interfaces
+				@interfaces = []
+				@interfaces_count.times { @interfaces << @file.read_u2 }
+			end
+
+			def read_fields
+				@fields = []
+				@fields_count.times { @fields << ItemInfo::FieldInfo.new(self, @file) }
+			end
+
+			def read_methods
+				@methods = []
+				@methods_count.times { @methods << ItemInfo::MethodInfo.new(self, @file) }
+			end
+
+			def read_attributes
+				@attributes = []
+				@attributes_count.times { @attributes << ItemInfo::AttributeInfoFactory.produce(self, self, @file) }
 			end
 		end
 
-		def name
-			@class_data.constant_pool.fetch(@name_index).bytes
-		end
-	end
-
-	class MethodInfo < BaseInfo
-		attr_accessor :access_flags, :name_index, :descriptor_index, :attributes_count, :attributes
-		def initialize(class_data, f)
-			super
-			@access_flags = f.read_u2
-			@name_index = f.read_u2
-			@descriptor_index = f.read_u2
-			@attributes_count = f.read_u2
-			@attributes = []
-			@attributes_count.times do
-				@attributes << AttributeInfoFactory.produce(self, class_data, f)
+		module ItemInfo
+			class BaseInfo
+				def initialize(class_data, f)
+					@class_data = class_data
+				end
 			end
-		end
 
-		def name
-			@class_data.constant_pool.fetch(@name_index).bytes
-		end
+			class FieldRefInfo < BaseInfo
+				attr_accessor :class_index, :name_and_type_index
 
-		def code
-			@code ||= @attributes.find {|a| a.is_a?(CodeAttributeInfo)}.code
-		end
-	end
+				def initialize(class_data, f)
+					super
+					@class_index = f.read_u2
+					@name_and_type_index = f.read_u2
+				end
+			end
 
-	class ConstantInfoFactory
-		@@classes = {
-			7 => ClassInfo,
-			9 => FieldRefInfo,
-			10 => MethodRefInfo,
-			11 => InterfaceMethodRefInfo,
-			8 => StringInfo,
-			3 => :CONSTANT_Integer,
-			4 => :CONSTANT_Float,
-			5 => :CONSTANT_Long,
-			6 => :CONSTANT_Double,
-			12 => NameAndTypeInfo,
-			1 => Utf8Info,
-			15 => :CONSTANT_MethodHandle,
- 			16 => :CONSTANT_MethodType,
-			18 => :CONSTANT_InvokeDynamic
-		}
+			class MethodRefInfo < FieldRefInfo
+			end
 
-		def self.produce(constant_type_id, class_data, file)
-			@@classes.fetch(constant_type_id).new(class_data, file)
+			class InterfaceMethodRefInfo < FieldRefInfo
+			end
+
+			class ClassInfo < BaseInfo
+				attr_accessor :name_index
+
+				def initialize(class_data, f)
+					super
+					@name_index = f.read_u2
+				end
+			end
+
+			class StringInfo < BaseInfo
+				attr_accessor :string_index
+
+				def initialize(class_data, f)
+					super
+					@string_index = f.read_u2
+				end
+			end
+
+			class Utf8Info < BaseInfo
+				attr_accessor :length, :bytes
+
+				def initialize(class_data, f)
+					super
+					@length = f.read_u2;
+					@bytes = f.read(@length)
+					#puts @bytes
+				end
+			end
+
+			class NameAndTypeInfo < BaseInfo
+				attr_accessor :name_index, :descriptor_index
+
+				def initialize(class_data, f)
+					super
+					@name_index = f.read_u2;
+					@descriptor_index = f.read_u2
+				end
+			end
+
+			class AttributeInfo < BaseInfo
+				def initialize(class_data, f)
+					super
+					@attribute_length = f.read_u4
+					read_info(f)
+				end
+
+				private
+				def read_info(f)
+					f.read(@attribute_length)
+				end
+			end
+
+			class CodeAttributeInfo < AttributeInfo
+				attr_accessor :max_stack, :max_locals, :code, :exception_table, :attributes
+
+				def read_info(f)
+					@max_stack = f.read_u2
+					@max_locals = f.read_u2
+					code_length = f.read_u4
+					@code = f.read(code_length)
+					exception_table_length = f.read_u2
+					f.read(exception_table_length)
+					attributes_count = f.read_u2
+					@attributes = []
+					attributes_count.times do
+						@attributes << AttributeInfoFactory.produce(self, @class_data, f)
+					end
+				end
+			end
+
+			class AttributeInfoFactory
+				def self.produce(caller_obj, class_data, f)
+					attribute_name_index = f.read_u2
+					attribute_name = class_data.constant_pool.fetch(attribute_name_index).bytes
+					case attribute_name
+					when "Code"
+						attribute_class = CodeAttributeInfo
+					else
+						puts "\033[1;36m" + "No class #{attribute_name} for #{caller_obj.class}" + "\033[0m"
+						attribute_class = AttributeInfo
+					end
+					return attribute_class.new(class_data, f)
+				end
+			end
+
+			class FieldInfo < BaseInfo
+				attr_accessor :access_flags, :name_index, :descriptor_index, :attributes_count, :attributes
+				def initialize(class_data, f)
+					super
+					@access_flags = f.read_u2
+					@name_index = f.read_u2
+					@descriptor_index = f.read_u2
+					@attributes_count = f.read_u2
+					@attributes = []
+					@attributes_count.times do
+						@attributes << AttributeInfoFactory.produce(self, class_data, f)
+					end
+				end
+
+				def name
+					@class_data.constant_pool.fetch(@name_index).bytes
+				end
+			end
+
+			class MethodInfo < BaseInfo
+				attr_accessor :access_flags, :name_index, :descriptor_index, :attributes_count, :attributes
+				def initialize(class_data, f)
+					super
+					@access_flags = f.read_u2
+					@name_index = f.read_u2
+					@descriptor_index = f.read_u2
+					@attributes_count = f.read_u2
+					@attributes = []
+					@attributes_count.times do
+						@attributes << AttributeInfoFactory.produce(self, class_data, f)
+					end
+				end
+
+				def name
+					@class_data.constant_pool.fetch(@name_index).bytes
+				end
+
+				def code
+					puts @attributes.collect {|a| a.class}
+					@code ||= @attributes.find {|a| a.is_a?(CodeAttributeInfo)}.code
+				end
+			end
+
+			class ConstantInfoFactory
+				@@classes = {
+					7 => ClassInfo,
+					9 => FieldRefInfo,
+					10 => MethodRefInfo,
+					11 => InterfaceMethodRefInfo,
+					8 => StringInfo,
+					3 => :CONSTANT_Integer,
+					4 => :CONSTANT_Float,
+					5 => :CONSTANT_Long,
+					6 => :CONSTANT_Double,
+					12 => NameAndTypeInfo,
+					1 => Utf8Info,
+					15 => :CONSTANT_MethodHandle,
+					16 => :CONSTANT_MethodType,
+					18 => :CONSTANT_InvokeDynamic
+				}
+
+				def self.produce(constant_type_id, class_data, file)
+					@@classes.fetch(constant_type_id).new(class_data, file)
+				end
+			end
 		end
 	end
 end
--- class_reader.rb
+++ class_reader.rb
@@ -1,237 +1,150 @@
 require 'class_file_items'
 require 'base_classes'
 
-module JavaClassFile
-	module BigEndianMethods
-		def read_u4
-			self.read(4).unpack("N").first
-		end
 
-		def read_u2
-			self.read(2).unpack("n").first
-		end
-
-		def read_u1
-			self.read(1).unpack("C").first
-		end
-	end
-	
-	class BigEndianFile < File
-		include BigEndianMethods
+class StackFrame
+	attr_accessor :pc, :code, :stack
+	def initialize(vm, code, constant_pool, prev_stack_frame = nil, vals_to_push = [])
+		@vm = vm
+		@code = code
+		@prev_stack_frame = prev_stack_frame
+		@constant_pool = constant_pool
+		@pc = 0
+		@stack = []
+		vals_to_push.each {|val| self.push(val) }
 	end
 
-	class ClassData
-		attr_reader :constant_pool, :fields, :methods
-
-		def read(file_path)
-			@file = BigEndianFile.open(file_path, "rb")
-					
-			read_magic
-			@minor = @file.read_u2
-			read_major
-			@constant_pool_count = @file.read_u2
-			read_constant_pool
-			@access_flags = @file.read_u2
-			@this_class = @file.read_u2
-			@super_class = @file.read_u2
-			@interfaces_count = @file.read_u2
-			read_interfaces
-			@fields_count = @file.read_u2
-			read_fields
-			@methods_count = @file.read_u2
-			read_methods
-			@attributes_count = @file.read_u2
-			read_attributes
-
-			@file.close
-		end
-
-		private
-		def read_magic
-			@magic = @file.read_u4
-			raise "Not a class file" if @magic != 0xCAFEBABE
-		end
-
-		def read_major
-			@major = @file.read_u2
-			raise "Not Java SE 6 class" if @major != 0x32
-		end
-
-		def read_constant_pool
-			@constant_pool = [nil] #zero index is not used, since indexing starts from 1 to pool_count - 1
-			(@constant_pool_count - 1).times do
-				constant_type_id = @file.read_u1
-				constant = ConstantInfoFactory.produce(constant_type_id, self, @file)
-				@constant_pool << constant
-			end
-		end
-
-		def read_interfaces
-			@interfaces = []
-			@interfaces_count.times { @interfaces << @file.read_u2 }
-		end
-
-		def read_fields
-			@fields = []
-			@fields_count.times { @fields << FieldInfo.new(self, @file) }
-		end
-
-		def read_methods
-			@methods = []
-			@methods_count.times { @methods << MethodInfo.new(self, @file) }
-		end
-
-		def read_attributes
-			@attributes = []
-			@attributes_count.times { @attributes << AttributeInfoFactory.produce(self, self, @file) }
-		end
+	def push(val)
+		@stack.push(val)
 	end
 
+	def pop
+		@stack.pop
+	end
 
-	class StackFrame
-		attr_accessor :pc, :code, :stack
-		def initialize(vm, code, constant_pool, prev_stack_frame = nil, vals_to_push = [])
-			@vm = vm
-			@code = code
-			@prev_stack_frame = prev_stack_frame
-			@constant_pool = constant_pool
-			@pc = 0
-			@stack = []
-			vals_to_push.each {|val| self.push(val) }
-		end
+	def top
+		@stack.last
+	end
 
-		def push(val)
-			@stack.push(val)
-		end
+	def get_index_arg
+		index1 = @code[@pc + 1]
+		index2 = @code[@pc + 2]
+		(index1 << 8) + index2
+	end
 
-		def pop
-			@stack.pop
-		end
+	def get_pc_arg
+		@code[(@pc + 1)..(@pc + 2)].reverse.unpack('s').first
+	end
 
-		def top
-			@stack.last
-		end
+	def exec
+		opcode = @code[@pc]
+		case opcode
+		when 0x03 #iconst_0
 
-		def get_index_arg
-			index1 = @code[@pc + 1]
-			index2 = @code[@pc + 2]
-			(index1 << 8) + index2
-		end
+			push(0)
+			@pc += 1
+		when 0x08 #iconst_5
 
-		def get_pc_arg
-			@code[(@pc + 1)..(@pc + 2)].reverse.unpack('s').first
-		end
+			push(5)
+			@pc += 1
+		when 0x3c #istore_1
 
-		def exec
-			opcode = @code[@pc]
-			case opcode
-			when 0x03 #iconst_0
-				
-				push(0)
-				@pc += 1
-			when 0x08 #iconst_5
+			@stack[1] = pop
+			@pc += 1
+		when 0x1b #iload_1
 
-				push(5)
-				@pc += 1
-			when 0x3c #istore_1
-				
-				@stack[1] = pop
-				@pc += 1
-			when 0x1b #iload_1
-				
-				push(@stack[1])
-				@pc += 1
-			when  0xb2 #getstatic
-				
-				index = get_index_arg
-				class_index = @constant_pool[index].class_index
-				name_and_type_index = @constant_pool[index].name_and_type_index
-				class_name_index = @constant_pool[class_index].name_index
-				method_name_index = @constant_pool[name_and_type_index].name_index
-				descriptor_index = @constant_pool[name_and_type_index].descriptor_index
-				class_name = @constant_pool[class_name_index].bytes
-				method_name = @constant_pool[method_name_index].bytes
-				descriptor = @constant_pool[descriptor_index].bytes
-				push(@vm.class_pool.get_class(class_name).static_fields.fetch(method_name))
-				@pc += 3
-			when 0xbb #new
-				index = get_index_arg
-				class_name_index = @constant_pool[index].name_index
-				class_name = @constant_pool[class_name_index].bytes
-				#klass = class_name.split("/").collect {|n| n.capitalize}.inject(Object) {|memo,name| memo = memo.const_get(name); memo}
-				push(JavaObject.new)
-				@pc += 3
-			when 0x59 #dup
-				
-				push(top)
-				@pc += 1
-			when 0xb7 #invokespecial
-				
-				index = get_index_arg
-				class_index = @constant_pool[index].class_index
-				name_and_type_index = @constant_pool[index].name_and_type_index
-				class_name_index = @constant_pool[class_index].name_index
-				method_name_index = @constant_pool[name_and_type_index].name_index
-				descriptor_index = @constant_pool[name_and_type_index].descriptor_index
-				class_name = @constant_pool[class_name_index].bytes
-				method_name = @constant_pool[method_name_index].bytes
-				descriptor = @constant_pool[descriptor_index].bytes
-				method_name += ":" + descriptor
-				method = @vm.class_pool.get_class(class_name).instance_methods.fetch(method_name)
-				args = []
-				method.arity.times { args << pop()}
-				args = args.reverse
-				method.call(*args)
-				@pc += 3
-			when 0x12 #ldc
-				
-				index = @code[@pc + 1]
-				constant = @constant_pool[index]
-				if constant.class == StringInfo
-					push(@constant_pool[constant.string_index].bytes)
-				else
-					raise "Unknown class"
-				end
-				@pc += 2
-			when 0xb6 #invokevirual
-				
-				index = get_index_arg
-				class_index = @constant_pool[index].class_index
-				name_and_type_index = @constant_pool[index].name_and_type_index
-				class_name_index = @constant_pool[class_index].name_index
-				method_name_index = @constant_pool[name_and_type_index].name_index
-				descriptor_index = @constant_pool[name_and_type_index].descriptor_index
-				class_name = @constant_pool[class_name_index].bytes
-				method_name = @constant_pool[method_name_index].bytes
-				descriptor = @constant_pool[descriptor_index].bytes
-				method_name += ":" + descriptor
-				method = @vm.class_pool.get_class(class_name).instance_methods.fetch(method_name)
-				
-				args = []
-				method.arity.times { args << pop()}
-				args = args.reverse
-				retval = method.call(*args)
-				push(retval) if retval.class != JavaReturnNothing
-				@pc += 3
-			when 0x84 #iinc
-				@stack[@code[@pc + 1]] += @code[@pc + 2]
-				@pc += 3
-			when 0xa1 #if_icmplt
-				index = get_pc_arg
-				val2 = pop
-				val1 = pop
-				if val1 < val2
-					@pc += index
-				else
-					@pc += 3
-				end
-			when 0xb1
-				return JavaReturnNothing
+			push(@stack[1])
+			@pc += 1
+		when  0xb2 #getstatic
+
+			index = get_index_arg
+			class_index = @constant_pool[index].class_index
+			name_and_type_index = @constant_pool[index].name_and_type_index
+			class_name_index = @constant_pool[class_index].name_index
+			method_name_index = @constant_pool[name_and_type_index].name_index
+			descriptor_index = @constant_pool[name_and_type_index].descriptor_index
+			class_name = @constant_pool[class_name_index].bytes
+			method_name = @constant_pool[method_name_index].bytes
+			descriptor = @constant_pool[descriptor_index].bytes
+			push(@vm.class_pool.get_class(class_name).static_fields.fetch(method_name))
+			@pc += 3
+		when 0xbb #new
+			index = get_index_arg
+			class_name_index = @constant_pool[index].name_index
+			class_name = @constant_pool[class_name_index].bytes
+			#klass = class_name.split("/").collect {|n| n.capitalize}.inject(Object) {|memo,name| memo = memo.const_get(name); memo}
+			push(JavaObject.new)
+			@pc += 3
+		when 0x59 #dup
+
+			push(top)
+			@pc += 1
+		when 0xb7 #invokespecial
+
+			index = get_index_arg
+			class_index = @constant_pool[index].class_index
+			name_and_type_index = @constant_pool[index].name_and_type_index
+			class_name_index = @constant_pool[class_index].name_index
+			method_name_index = @constant_pool[name_and_type_index].name_index
+			descriptor_index = @constant_pool[name_and_type_index].descriptor_index
+			class_name = @constant_pool[class_name_index].bytes
+			method_name = @constant_pool[method_name_index].bytes
+			descriptor = @constant_pool[descriptor_index].bytes
+			method_name += ":" + descriptor
+			method = @vm.class_pool.get_class(class_name).instance_methods.fetch(method_name)
+			args = []
+			method.arity.times { args << pop()}
+			args = args.reverse
+			method.call(*args)
+			@pc += 3
+		when 0x12 #ldc
+
+			index = @code[@pc + 1]
+			constant = @constant_pool[index]
+			if constant.class == Java::ClassFile::ItemInfo::StringInfo
+				push(@constant_pool[constant.string_index].bytes)
 			else
-				raise "Unknown OPCODE: " + opcode.to_s(16)
+				raise "Unknown class"
 			end
-			exec
+			@pc += 2
+		when 0xb6 #invokevirual
+
+			index = get_index_arg
+			class_index = @constant_pool[index].class_index
+			name_and_type_index = @constant_pool[index].name_and_type_index
+			class_name_index = @constant_pool[class_index].name_index
+			method_name_index = @constant_pool[name_and_type_index].name_index
+			descriptor_index = @constant_pool[name_and_type_index].descriptor_index
+			class_name = @constant_pool[class_name_index].bytes
+			method_name = @constant_pool[method_name_index].bytes
+			descriptor = @constant_pool[descriptor_index].bytes
+			method_name += ":" + descriptor
+			method = @vm.class_pool.get_class(class_name).instance_methods.fetch(method_name)
+
+			args = []
+			method.arity.times { args << pop()}
+			args = args.reverse
+			retval = method.call(*args)
+			push(retval) if retval.class != JavaReturnNothing
+			@pc += 3
+		when 0x84 #iinc
+			@stack[@code[@pc + 1]] += @code[@pc + 2]
+			@pc += 3
+		when 0xa1 #if_icmplt
+			index = get_pc_arg
+			val2 = pop
+			val1 = pop
+			if val1 < val2
+				@pc += index
+			else
+				@pc += 3
+			end
+		when 0xb1
+			return JavaReturnNothing
+		else
+			raise "Unknown OPCODE: " + opcode.to_s(16)
 		end
+		exec
 	end
 end
 
@@ -240,18 +153,16 @@ class VirtualMachine
 	def initialize
 		@class_pool = ClassPool.new
 		@class_pool.load_classpath
-		
-		class_data = JavaClassFile::ClassData.new
 	end
 end
 
 
-class_data = JavaClassFile::ClassData.new
-class_data.read("DoWhileExample.class")
+class_definition = Java::ClassFile::ClassDefinition.new
+class_definition.read("DoWhileExample.class")
 
-main_method = class_data.methods.find {|m| m.name == "main"}
+main_method = class_definition.methods.find {|m| m.name == "main"}
 
-JavaClassFile::StackFrame.new(VirtualMachine.new, main_method.code, class_data.constant_pool, nil, ["aaa", nil]).exec
+StackFrame.new(VirtualMachine.new, main_method.code, class_definition.constant_pool, nil, ["aaa", nil]).exec
 
 #puts m.name
 #m.attributes.each {|a| puts a.name }
